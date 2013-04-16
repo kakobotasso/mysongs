829647ad619669748b62db00c3cf27fb39d1d32d
Kako Botasso <kakodev@gmail.com>
2013-04-15 22:31:32 -0300

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,26 @@
+source 'https://rubygems.org'
+
+gem 'rails'
+
+group :assets do
+  gem 'sass-rails'
+  gem 'coffee-rails'
+  gem 'therubyracer', :platforms => :ruby
+  gem 'uglifier'
+end
+
+gem 'jquery-rails'
+
+group :development, :test do
+  gem 'mysql2'
+  gem 'rspec-rails'
+  gem 'guard'
+  gem 'guard-rails'
+  gem 'guard-rspec'
+  gem 'simplecov'
+  gem 'rb-inotify'
+  gem 'capybara'
+  gem 'factory_girl'
+end
+
+gem 'pg'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mysongs::Application.config.secret_token = 'dac2015ebde1a95ce790ba32c29938c7920f7dfbc831cc0ba78753ceec049d0e15363309da006d8a00b0de19600c7f83a22cba3442873d430c67c0d850613449'
